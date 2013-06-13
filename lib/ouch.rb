b9125009bf3419ae5af0d4479ec04506dea2df84
dickeyxxx <jeff@dickey.xxx>
2013-06-13 15:27:54 -0700

    cleanup and handle when no hospitals are found

--- lib/ouch.rb
+++ lib/ouch.rb
@@ -3,6 +3,26 @@ require "ouch/info"
 
 module Ouch
   def self.find(lat: nil, lng: nil)
-    Info.new(lat: lat, lng: lng)
+    conn = Faraday.new("http://www.ushospitalfinder.com")
+    response = conn.get '/hospitals/search', { search_query: 'hospital', lat: lat, lng: lng }
+    page = Nokogiri::HTML.parse(response.body)
+    link = page.css('#list tr:first a').first
+    return unless link
+    url = link[:href]
+    response = conn.get(url)
+    page = Nokogiri::HTML.parse(response.body)
+    details = page.css('#detail-center p:has(b)')
+    data = details.inject({}) do |h, detail|
+      h[detail.children[1].children[0].text] = detail.children[2].text.split.join(' ').strip
+      h
+    end
+
+    {
+      name: data["Name:"],
+      address: data["Address:"],
+      phone: data["Phone:"],
+      number_of_beds: data["Number of Beds:"].to_i,
+      url: "http://www.ushospitalfinder.com#{url}",
+    }
   end
 end
--- test/ouch_test.rb
+++ test/ouch_test.rb
@@ -7,22 +7,26 @@ class TestOuch < MiniTest::Unit::TestCase
   end
 
   def test_name
-    @hospital.name.must_equal "Cedars-Sinai Medical Center"
+    @hospital[:name].must_equal "Cedars-Sinai Medical Center"
   end
 
   def test_address
-    @hospital.address.must_equal "8700 Beverly Boulevard Los Angeles, CA 90048-1865"
+    @hospital[:address].must_equal "8700 Beverly Boulevard Los Angeles, CA 90048-1865"
   end
 
   def test_phone
-    @hospital.phone.must_equal "310-423-5000"
+    @hospital[:phone].must_equal "310-423-5000"
   end
 
   def test_number_of_beds
-    @hospital.number_of_beds.must_equal 914
+    @hospital[:number_of_beds].must_equal 914
   end
 
   def test_url
-    @hospital.url.must_equal 'http://www.ushospitalfinder.com/hospital/Cedars-Sinai-Medical-Center-Los-Angeles-CA'
+    @hospital[:url].must_equal 'http://www.ushospitalfinder.com/hospital/Cedars-Sinai-Medical-Center-Los-Angeles-CA'
+  end
+
+  def test_north_pole
+    Ouch.find(lat: 0, lng: 0).must_equal nil
   end
 end
